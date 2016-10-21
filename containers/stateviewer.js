86354f5f27ba697041d95b4e052b6a5a22f63998
HenrikGr <henrik.gronvall@bredband.net>
2016-10-21 23:26:39 +0200

    Updated documentation

--- containers/stateviewer.js
+++ containers/stateviewer.js
@@ -1,16 +1,24 @@
+/*!
+ * Description: The application module for this playground.
+ *
+ *
+ * Author:  Henrik Grönvall
+ * File:
+ * Version: 0.0.1
+ * Created on 2016-10-16
+ */
 import { connect } from 'react-redux';
 import StateViewer from '../components/stateviewer';
 import stateHistory from '../statehistory';
 
-const mapStateToProps = state => ({
-  books:         state.books,
-  topic:         state.topic,
-  currentStatus: state.currentStatus,
-  displayMode:   state.displayMode,
-  history:       stateHistory,
-});
+const mapStateToProps = (state) => {
+  return {
+    books: state.books,
+    topic: state.topic,
+    currentStatus: state.currentStatus,
+    displayMode: state.displayMode,
+    history: stateHistory
+  }
+};
 
-export default connect(
-  mapStateToProps,
-  null
-)(StateViewer);
+export default connect(mapStateToProps,null)(StateViewer);
