ef979d6641b9d70daaf6bba0424083b30e571a1b
HenrikGr <henrik.gronvall@bredband.net>
2016-10-22 01:21:24 +0200

    Updated documentation

--- reducers.js
+++ reducers.js
@@ -7,19 +7,31 @@
  * - Can act on all or part of the state
  * - Can be combined and reused
  *
- * Because they're pure functions, reducers have no side effects —
- * so they're easy to read,
- * test, and debug. And you can compose reducers, which makes it easy to implement simple
- * reducers that are concerned with only a portion of the overall application state.
+ * Because they're pure functions, reducers have no side effects,
+ * so they're easy to read, test, and debug. And you can compose reducers,
+ * which makes it easy to implement simple reducers that are concerned
+ * with only a portion of the overall application state.
  *
- * Because application state is immutable and because reducers are pure functions with
- * no side effects.
-  *
- * Redux is a predictable state container: Given any state and any action, you can predict
- * the next state of the application with absolute certainty.
+ * Redux is a predictable state container: Given any state and any action,
+ * you can predict the next state of the application with absolute certainty.
  *
- * That predictable element of Redux is the killer feature that opens the door to infinite
- * undo/redo and live-editing time travel.
+ * That predictable element of Redux is the killer feature that opens the
+ * door to infinite undo/redo and live-editing time travel.
+ *
+ * Recall that when you dispatch an action by calling the Redux dispatch() function,
+ * Redux calls the application's reducer function, passing the current state and the action.
+ * Actions, which specify a new state, typically have a type. The action types are
+ * - SET_TOPIC and
+ * - SET_DISPLAY_MODE.
+ *
+ * Actions of the SET_TOPIC type have a topic property, and
+ * actions of the SET_DISPLAY_MODE type have a displayMode property,
+ * each specifying its new state.
+ *
+ * If you send an action to a reducer, and the reducer isn't interested —
+ * for instance, you send a SET_DISPLAY_MODE action to the topic reducer —
+ * the reducer returns the current state, unchanged.
+ *
  *
  *
  * Author:  Henrik Grönvall
@@ -43,9 +55,7 @@ const fetchReducer = (state = defaults.STATE, action) => {
       return [];
 
     case 'FETCH_FAILED':
-      alert('Fetch failed. Check your internet connection ' +
-            'or change the query.');
-
+      alert('Fetch failed. Check your internet connection or change the query.');
       return [];
 
     case 'FETCH_COMPLETE':
@@ -122,7 +132,6 @@ const undo = reducer => (state = stateHistory.present, action) => {
 };
 
 // Combine reducers
-
 export default undo(combineReducers({
   books: fetchReducer,
   topic: topicReducer,
