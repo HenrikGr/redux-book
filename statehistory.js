10aef5492d4ab7681151db42977fc6b6931689f6
HenrikGr <henrik.gronvall@bredband.net>
2016-10-22 01:17:51 +0200

    Updated documentation

--- statehistory.js
+++ statehistory.js
@@ -1,41 +1,44 @@
+/*!
+ * Description:
+ *
+ *
+ * Author:  Henrik Grönvall
+ * File:
+ * Version: 0.0.1
+ * Created on 2016-10-16
+ */
 export default {
   past: [],
   present: undefined,
   future: [],
 
-  thereIsAPresent:     function () { return this.present !== undefined; },
-  thereIsAPast:        function () { return this.past.length > 0; },
-  thereIsAFuture:      function () { return this.future.length > 0; },
-  setPresent:          function (state) { this.present = state; },
-  movePresentToPast:   function () { this.past.push(this.present); },
- 
-  push: function (currentState) {
+  thereIsAPresent() { return this.present !== undefined; },
+  thereIsAPast() { return this.past.length > 0; },
+  thereIsAFuture() { return this.future.length > 0; },
+  setPresent(state) { this.present = state; },
+  movePresentToPast() { this.past.push(this.present); },
+  push(currentState) {
     if (this.thereIsAPresent()) {
       this.movePresentToPast();
     }
     this.setPresent(currentState);
   },
-  
-  getIndex: function () {
+  getIndex() {
     return this.past.length;
   },
-  
-  undo: function () {
-    if (this.thereIsAPast()) {   
+  undo() {
+    if (this.thereIsAPast()) {
       this.gotoState(this.getIndex() - 1);
     }
-  },  
-
-  redo: function () {
+  },
+  redo() {
     if (this.thereIsAFuture()) {
       this.gotoState(this.getIndex() + 1);
     }
   },
-
-  gotoState: function (i) {
+  gotoState(i) {
     const index = Number(i);
     const allstates = [...this.past, this.present, ...this.future];
-   
     this.present = allstates[index];
     this.past = allstates.slice(0, index);
     this.future = allstates.slice(index + 1, allstates.length);
