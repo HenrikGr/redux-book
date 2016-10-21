86a6cc82f34a7631d70a5abbd43acee078ecdd2f
HenrikGr <henrik.gronvall@bredband.net>
2016-10-21 23:26:01 +0200

    Updated documentation

--- containers/books.js
+++ containers/books.js
@@ -1,14 +1,22 @@
+/*!
+ * Description:
+ *
+ * Author:  Henrik Grönvall
+ * File:
+ * Version: 0.0.1
+ * Created on 2016-10-16
+ */
 import { connect } from 'react-redux';
 import Books from '../components/books';
 
-const mapStateToProps = state => ({
-  books:         state.books,
-  topic:         state.topic,
-  currentStatus: state.currentStatus,
-  displayMode:   state.displayMode,
-});
+// Map state to props
+const mapStateToProps = (state) => {
+  return {
+    books: state.books,
+    topic: state.topic,
+    currentStatus: state.currentStatus,
+    displayMode: state.displayMode
+  }
+};
 
-export default connect(
-  mapStateToProps,
-  null
-)(Books);
+export default connect(mapStateToProps,null)(Books);
