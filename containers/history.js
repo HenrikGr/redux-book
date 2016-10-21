4f51d5fa55f6119d7e72a12247f77affcb0148d2
HenrikGr <henrik.gronvall@bredband.net>
2016-10-21 23:27:04 +0200

    Updated documentation

--- containers/history.js
+++ containers/history.js
@@ -1,29 +1,39 @@
+/*!
+ * Description: The application module for this playground.
+ * 
+ *
+ * Author:  Henrik Grönvall
+ * File:
+ * Version: 0.0.1
+ * Created on 2016-10-16
+ */
 import { connect } from 'react-redux';
 import { undo, redo, gotoState } from '../actions';
 import { History } from '../components/history';
 import stateHistory from '../statehistory';
 
-const mapStateToProps = () => ({
-  past: stateHistory.past,
-  present: stateHistory.present,
-  future: stateHistory.future,
-});
+const mapStateToProps = () => {
+  return {
+    past: stateHistory.past,
+    present: stateHistory.present,
+    future: stateHistory.future
+  }
+};
 
-const mapDispatchToProps = dispatch => ({
-  undo: () => {
-    dispatch(undo());
-  },
+const mapDispatchToProps = (dispatch) => {
+  return {
+    undo: () => {
+      dispatch(undo());
+    },
+  
+    redo: () => {
+      dispatch(redo());
+    },
+  
+    gotoState: (stateIndex) => {
+      dispatch(gotoState(stateIndex));
+    }
+  }
+};
 
-  redo: () => {
-    dispatch(redo());
-  },
-
-  gotoState: stateIndex => {
-    dispatch(gotoState(stateIndex));
-  },
-});
-
-export default connect (
-  mapStateToProps,
-  mapDispatchToProps
-)(History);
+export default connect (mapStateToProps,mapDispatchToProps)(History);
