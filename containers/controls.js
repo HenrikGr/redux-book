f1a778750980396cf9371efbfbd87870735a795c
HenrikGr <henrik.gronvall@bredband.net>
2016-10-22 01:20:04 +0200

    Updated documentation

--- containers/controls.js
+++ containers/controls.js
@@ -1,14 +1,20 @@
+/*!
+ * Description:
+ *
+ *
+ * Author:  Henrik Grönvall
+ * File:
+ * Version: 0.0.1
+ * Created on 2016-10-16
+ */
 import { connect } from 'react-redux';
 import Controls from '../components/controls';
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   topic: state.topic,
   displayMode: state.displayMode,
 });
 
 const mapDispatchToProps = null;
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Controls);
+export default connect(mapStateToProps, mapDispatchToProps)(Controls);
