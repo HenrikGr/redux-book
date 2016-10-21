17996081c0567572f5c237a1086f1d12f738deee
HenrikGr <henrik.gronvall@bredband.net>
2016-10-22 01:20:15 +0200

    Updated documentation

--- containers/displayMode.js
+++ containers/displayMode.js
@@ -1,20 +1,25 @@
+/*!
+ * Description:
+ *
+ *
+ * Author:  Henrik Grönvall
+ * File:
+ * Version: 0.0.1
+ * Created on 2016-10-16
+ */
 import { connect } from 'react-redux';
 import { setDisplayMode } from '../actions';
 import DisplayMode from '../components/displayMode';
 
 const mapStateToProps = null;
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   setListing: () => {
     dispatch(setDisplayMode('LISTING'));
   },
-
   setThumbnail: () => {
     dispatch(setDisplayMode('THUMBNAIL'));
   },
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(DisplayMode);
+export default connect(mapStateToProps, mapDispatchToProps)(DisplayMode);
