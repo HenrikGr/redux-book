0b927b2510fca17e92c4c76c97830d9a6936b9b7
HenrikGr <henrik.gronvall@bredband.net>
2016-10-22 23:40:51 +0200

    Updated documentation

--- containers/history.js
+++ containers/history.js
@@ -1,5 +1,14 @@
 /*!
- * Description:
+ * Description: History container component.
+ *
+ * We are using mapStateToProps to subscribe to Redux store updates. Any time
+ * the store will updates, mapStoreToProps will be called and pass the changed state
+ * as props to the TopicSelector component.
+ *
+ * We are also using mapDispatchToProps to pass action creators as props to the
+ * TopicSelector component. The object we are passing include functions with
+ * the same names, but with every action creator wrapped into a dispatch call
+ * so they may be invoked directly.
  *
  *
  * Author:  Henrik Grönvall
@@ -7,17 +16,26 @@
  * Version: 0.0.1
  * Created on 2016-10-16
  */
+// Module dependencies
 import { connect } from 'react-redux';
+
+// Import action creators
 import { undo, redo, gotoState } from '../actions';
+
+// Import History component
 import { History } from '../components/history';
+
+// Import stateHistory object
 import stateHistory from '../statehistory';
 
+// Map state changes to props
 const mapStateToProps = () => ({
   past: stateHistory.past,
   present: stateHistory.present,
   future: stateHistory.future,
 });
 
+// Map action creators as props
 const mapDispatchToProps = (dispatch) => ({
   undo: () => {
     dispatch(undo());
@@ -30,4 +48,7 @@ const mapDispatchToProps = (dispatch) => ({
   },
 });
 
+/**
+ * Export the History container component
+ */
 export default connect(mapStateToProps, mapDispatchToProps)(History);
