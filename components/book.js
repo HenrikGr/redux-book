4a71c7ca6eca648e62d6449f1fcffac4178b58c4
HenrikGr <henrik.gronvall@bredband.net>
2016-10-23 00:09:44 +0200

    Updated documentation

--- components/book.js
+++ components/book.js
@@ -1,5 +1,7 @@
 /*!
- * Description:
+ * Description: Book component
+ *
+ * The component render a book either as a thumbnail or as a link.
  *
  *
  * Author:  Henrik Grönvall
@@ -7,25 +9,25 @@
  * Version: 0.0.1
  * Created on 2016-10-16
  */
+// Module dependencies
 import React from 'react';
 
-const Book = (props) => {
-  const { item, displayMode } = props;
+// Book component
+const Book = ({item,displayMode}) => {
   const styles = {
     links: {
       marginTop: '20px',
     },
-
     link: {
       padding: '25px',
     },
-
     image: {
       boxShadow: '3px 3px 3px #686868',
       marginBottom: '15px',
     },
   };
 
+  // Helper function to render book data as link
   const linkMarkup = (currentItem, link) => (
     <div style={styles.links}>
       <a href={link} style={styles.link}>
@@ -34,6 +36,7 @@ const Book = (props) => {
     </div>
   );
 
+  // Helper function to render book data as a thumbnail
   const thumbnailMarkup = (currentItem, link) => (
     <a href={link} style={styles.link}>
       <img
@@ -44,14 +47,22 @@ const Book = (props) => {
     </a>
   );
 
+  // Get the link from book data
   const link = item.volumeInfo.canonicalVolumeLink;
 
+  // Render component
   return displayMode === 'THUMBNAIL' ? thumbnailMarkup(item, link) : linkMarkup(item, link);
 };
 
+// Book component props
 Book.propTypes = {
+  // Book data
   item: React.PropTypes.object.isRequired,
+  // Display mode
   displayMode: React.PropTypes.string.isRequired,
 };
 
+/**
+ * Export the Book component
+ */
 export default Book;
