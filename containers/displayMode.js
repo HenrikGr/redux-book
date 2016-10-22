217d25975ba1791d58dd389df71ae16f36271d40
HenrikGr <henrik.gronvall@bredband.net>
2016-10-22 23:43:41 +0200

    Updated documentation

--- containers/displayMode.js
+++ containers/displayMode.js
@@ -1,5 +1,10 @@
 /*!
- * Description:
+ * Description: DisplayMode container component.
+ *
+ * We are also using mapDispatchToProps to pass action creators as props to the
+ * TopicSelector component. The object we are passing include functions with
+ * the same names, but with every action creator wrapped into a dispatch call
+ * so they may be invoked directly.
  *
  *
  * Author:  Henrik Grönvall
@@ -7,12 +12,19 @@
  * Version: 0.0.1
  * Created on 2016-10-16
  */
+// Module dependencies
 import { connect } from 'react-redux';
+
+// Import action creators
 import { setDisplayMode } from '../actions';
+
+// Import DisplayMode component
 import DisplayMode from '../components/displayMode';
 
+// No map state to props
 const mapStateToProps = null;
 
+// Map action creators to props
 const mapDispatchToProps = (dispatch) => ({
   setListing: () => {
     dispatch(setDisplayMode('LISTING'));
@@ -22,4 +34,7 @@ const mapDispatchToProps = (dispatch) => ({
   },
 });
 
+/**
+ * export the DisplayMode container component
+ */
 export default connect(mapStateToProps, mapDispatchToProps)(DisplayMode);
