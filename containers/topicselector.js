0d495af131452eb22e46db1027e2e3d37146f68b
HenrikGr <henrik.gronvall@bredband.net>
2016-10-22 23:33:18 +0200

    Updated documentation

--- containers/topicselector.js
+++ containers/topicselector.js
@@ -1,5 +1,15 @@
 /*!
- * Description:
+ * Description: TopicSelector container component
+ *
+ * We are using mapStateToProps to subscribe to Redux store updates. Any time
+ * the store will updates, mapStoreToProps will be called and pass the changed state
+ * as props to the TopicSelector component.
+ *
+ * We are also using mapDispatchToProps to pass action creators as props to the
+ * TopicSelector component. The object we are passing include functions with
+ * the same names, but with every action creator wrapped into a dispatch call
+ * so they may be invoked directly.
+ *
  *
  *
  * Author:  Henrik Grönvall
@@ -11,10 +21,12 @@ import { connect } from 'react-redux';
 import { setTopic, fetchBooks } from '../actions';
 import TopicSelector from '../components/topicselector';
 
+// Map state changes to props
 const mapStateToProps = (state) => ({
   topic: state.topic,
 });
 
+// Map action creators as props
 const mapDispatchToProps = (dispatch) => ({
   setTopic: (topic) => {
     dispatch(setTopic(topic));
@@ -25,4 +37,7 @@ const mapDispatchToProps = (dispatch) => ({
   },
 });
 
+/**
+ * Export the TopicSelector container component.
+ */
 export default connect(mapStateToProps, mapDispatchToProps)(TopicSelector);
