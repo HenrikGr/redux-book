283a848027a9c16aecc1e7bffcd379c833a7b4ce
HenrikGr <henrik.gronvall@bredband.net>
2016-10-23 22:53:18 +0200

    Updated documentation

--- containers/topicselector.js
+++ containers/topicselector.js
@@ -5,10 +5,17 @@
  * the store will updates, mapStoreToProps will be called and pass the changed state
  * as props to the TopicSelector component.
  *
+ * The props we will pass are;
+ * - topic, string containing the text the user entered
+ *
  * We are also using mapDispatchToProps to pass action creators as props to the
  * TopicSelector component. The object we are passing include functions with
  * the same names, but with every action creator wrapped into a dispatch call
  * so they may be invoked directly.
+ *
+ * The action creators we pass as props are;
+ * - setTopic()
+ * - fetchTopic()
  *
  *
  * Author:  Henrik Grönvall
