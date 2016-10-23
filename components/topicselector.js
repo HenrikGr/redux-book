6878ac59549c19144b7413dde42458a0dd2315d7
HenrikGr <henrik.gronvall@bredband.net>
2016-10-23 22:52:56 +0200

    Updated documentation

--- components/topicselector.js
+++ components/topicselector.js
@@ -1,6 +1,13 @@
 /*!
  * Description: TopicSelector component
  *
+ * The component renders an input field to enter search terms
+ * utilizing the Google API.
+ *
+ * The setTopic() props is used to bind the input value to the
+ * Redux store and when pressing Enter key, we use the prop fetchTopic()
+ * to do a search for books.
+ *
  *
  * Author:  Henrik Grönvall
  * File:
@@ -15,12 +22,12 @@ import React from 'react';
 export default class TopicSelector extends React.Component {
   constructor(props) {
     super(props);
-    this.props = props;
 
     this.handleChange = this.handleChange.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
+  // Put the cursor at the end of the input field
   componentDidMount() {
     function putCursorAtEnd(input) {
       const value = input.value;
@@ -34,10 +41,12 @@ export default class TopicSelector extends React.Component {
     putCursorAtEnd(input);
   }
 
+  // Update topic state
   handleChange(event) {
     this.props.setTopic(event.target.value);
   }
 
+  // Perform a fetch if Enter
   handleKeyPress(event) {
     if (event.key === 'Enter') {
       this.props.fetchTopic(event.target.value);
@@ -80,6 +89,10 @@ export default class TopicSelector extends React.Component {
   }
 }
 
+/**
+ * TopicSelector props
+ * @type {{topic: *, setTopic: *, fetchTopic: *}}
+ */
 TopicSelector.propTypes = {
   topic: React.PropTypes.string.isRequired,
   setTopic: React.PropTypes.func.isRequired,
