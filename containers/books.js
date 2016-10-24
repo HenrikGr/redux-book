77f3116e5a9bcf8c0503a2ee2261f35947a043b1
HenrikGr <henrik.gronvall@bredband.net>
2016-10-24 18:09:32 +0200

    Updated documentation

--- containers/books.js
+++ containers/books.js
@@ -1,6 +1,26 @@
 /*!
  * Description: Books container component
  *
+ * The component is a container component with the purpose of subscribe
+ * to the Redux store and pass changes as props to the DisplayMode component.
+ *
+ * The app component hierarchy looks like this:
+ *
+ * App
+ *  - ControlsContainer
+ *   - TopicSelectorContainer
+ *    - TopicSelector
+ *   - DisplayModeContainer
+ *    - DisplayMode
+ *   - HistoryContainer
+ *    - History
+ *  - BooksContainer (*)
+ *   - Books
+ *    - Book
+ *    - Book
+ *   - ...
+ *  - StateViewerContainer
+ *
  * We are using mapStateToProps to subscribe to Redux store updates. Any time
  * the store will updates, mapStoreToProps will be called and pass the changed state
  * as props to the Books component. The props passed to the Book component are;
