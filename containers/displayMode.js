c64c622a86e2931766a9248001626f70fe84b85d
HenrikGr <henrik.gronvall@bredband.net>
2016-10-24 17:58:00 +0200

    Updated documentation

--- containers/displayMode.js
+++ containers/displayMode.js
@@ -1,10 +1,40 @@
 /*!
- * Description: DisplayMode container component.
+ * Description: DisplayModeContainer component.
+ *
+ * The component is a container component with the purpose of subscribe
+ * to the Redux store and pass changes as props to the DisplayMode component.
+ *
+ * The app component hierarchy looks like this:
+ *
+ * App
+ *  - ControlsContainer
+ *   - TopicSelectorContainer
+ *    - TopicSelector
+ *   - DisplayModeContainer (*)
+ *    - DisplayMode
+ *   - HistoryContainer
+ *    - History
+ *  - BooksContainer
+ *   - Book
+ *   - Book
+ *   - ...
+ *  - StateViewerContainer
+ *
+ * We are using mapStateToProps to subscribe to Redux store updates. Any time
+ * the store will updates, mapStoreToProps will be called and pass the changed state
+ * as props to the DisplayMode component.
+ *
+ * The props we will pass are;
+ * - displayMode, string containing the text the user entered
  *
  * We are also using mapDispatchToProps to pass action creators as props to the
  * DisplayMode component. The object we are passing include functions with
  * the same names, but with every action creator wrapped into a dispatch call
  * so they may be invoked directly.
+ *
+ * The action creators we pass as props are;
+ * - setListing()
+ * - setThumbnail()
  *
  *
  * Author:  Henrik Grönvall
@@ -21,6 +51,11 @@ import { setDisplayMode } from '../actions';
 // Import DisplayMode component
 import DisplayMode from '../components/displayMode';
 
+// Map state changes to props
+const mapStateToProps = (state) => ({
+  displayMode: state.displayMode,
+});
+
 // Map action creators to props
 const mapDispatchToProps = (dispatch) => ({
   setListing: () => {
@@ -32,6 +67,6 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 /**
- * export the DisplayMode container component
+ * export the DisplayModeContainer component
  */
-export default connect(null, mapDispatchToProps)(DisplayMode);
+export default connect(mapStateToProps, mapDispatchToProps)(DisplayMode);
